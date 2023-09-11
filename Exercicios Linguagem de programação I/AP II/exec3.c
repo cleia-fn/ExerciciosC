a79ba7dc015ac2f5cd1fbc253f1d7dc8cb9a21c5
l3omc <leo.carvalho95@yahoo.com.br>
2023-09-10 21:50:24 -0300

    exec 3

--- Exercicios Linguagem de programação I/AP II/exec3.c
+++ Exercicios Linguagem de programação I/AP II/exec3.c
@@ -10,17 +10,17 @@ onde i é fornecida pelo usuário.
 int main()
 {
 
-int i = 0,j = 0 , k = 0;
+int i = 0,j = 0 , k = 0, linsom;
 
 printf("Olá, por favor digite o tamanho desejado da sua matriz : ");
 scanf("%d", &k);
 
-float aux[k][k];    
+float aux[k][k], soma = 0;    
 
     for( i = 0 ; i < k; i++) 
     {
     	for (j = 0; j < k; j++){
-    		printf("digite o numero desejado na linha %d, coluna %d da sua matriz %d x %d : ", i,j, k,k);
+    		printf("digite o numero desejado na linha %d, coluna %d da sua matriz %d x %d : ", i+1,j+1, k,k);
     		scanf("%2f", &aux[i][j]);
     	
       
@@ -33,11 +33,23 @@ float aux[k][k];
      /*
      printf(" | \n");*/
     	for (j = 0; j < k; j++){
-    		printf("| %.2f ", aux[i][j]);
+    		
+    		printf(" | %.2f ", aux[i][j]);
     		
     	}
-    	printf("|\n");
+    	printf("| Linha %d \n", i+1);
+    	
     }
-    printf("\n\n\n");
+printf("\n\n\n");
+printf("Digite a linha que deseja que seja somada dentro da matriz %dx%d :  ", k,k);
+scanf("%d",&linsom);
+printf("a linha deseja contem \n\n");
+    for (j = 0; j < k; j++){
+    	printf(" %.2f ", aux[linsom -1][j]);
+    	soma = soma + aux[linsom-1][j];
+    }
+printf("\n\n a soma é  : %.2f  \n", soma);
+    
+    
     return 0;
 }
